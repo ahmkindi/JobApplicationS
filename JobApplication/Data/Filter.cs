38b1d3e1cbd52d10733fc2e4029ba94d6d3334cb
Ahmed Al Kindi <ahmkind19@gmail.com>
2021-04-28 21:43:57 +0400

    Add project files.

---
+++ JobApplication/Data/Filter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobApplication.Data
+{
+    public class Filter
+    {
+        public IList<string> Words { get; set; } = new List<string>();
+        public IList<string> Sentences { get; set; } = new List<string>();
+        public int Found { get; set; } = 0;
+        public bool Finished { get; set; } = false;
+
+        public Filter()
+        {
+            Words.Add(string.Empty);
+        }
+    }
+}
---
+++ JobApplication/Data/Resume.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobApplication.Data
+{
+    public class Resume
+    {
+        public string FileName { get; set; }
+        public bool FileUploaded { get; set; } = false;
+        public string UploadText { get; set; } = "Upload Resume";
+        public IList<IList<string>> Sentences { get; set; } = new List<IList<string>>();
+
+    }
+}
